7bf6f079a133d496b52fa603fe02b4dc944671d5
oyarukhi <olga.yarukhina@gmail.com>
2023-03-17 18:37:09 +0200

    get one user posts

--- pkg/posts.go
+++ pkg/posts.go
@@ -2,14 +2,13 @@ package models
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
 )
 
 type PostModel struct {
 	DB *sql.DB
 }
 
-// Insert - Метод для создания
 func (m *PostModel) Insert(header, description string) error {
 	stmt := `INSERT INTO post (header, description, created_at)
     VALUES(?, ?, current_date)`
@@ -21,10 +20,37 @@ func (m *PostModel) Insert(header, description string) error {
 
 	id, err := result.LastInsertId()
 	if err != nil {
-		return  err
+		return err
 	}
-	fmt.Println(id)
+	log.Println(id)
 
 	return nil
 }
 
+// Get one user posts
+func (m *PostModel) GetUserPosts(userID int) map[string][]string {
+	var header, description, created_at string
+	var allPosts map[string][]string
+
+	rows, err := m.DB.Query(`SELECT header, description, created_at from post where user_id = $userID`)
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+	
+
+	for rows.Next() {
+		err := rows.Scan(&header, &description, &created_at)
+		if err != nil {
+			log.Println("Something wrong with db")
+			return nil
+		}
+
+		var val []string
+		val = append(val, description)
+		val = append(val, created_at)
+		allPosts[header] = val
+	}
+
+	return allPosts
+}
